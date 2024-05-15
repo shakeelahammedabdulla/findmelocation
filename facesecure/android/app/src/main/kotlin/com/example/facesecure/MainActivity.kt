b2aaf3224d17e85ee4e044186c250ddcb11e3b9a
Shakeel Ahammed <100762038+shakeelahammedabdulla@users.noreply.github.com>
2024-05-15 13:06:26 +0530

    Delete facesecure directory

--- facesecure/android/app/src/main/kotlin/com/example/facesecure/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.facesecure
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- facesecure/lib/common/utils/extensions/size_extension.dart
+++
@@ -1,7 +0,0 @@
-import 'package:facesecure/common/utils/screen_size_util.dart';
-
-extension SizeExtension on num {
-  double get sw => ScreenSizeUtil.screenWidth * this;
-
-  double get sh => ScreenSizeUtil.screenHeight * this;
-}
